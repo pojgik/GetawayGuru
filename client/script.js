a4607d8e4f342d8325a77b8ca20739fb33294b15
Luke Dinkla <luke.dinkla@gmail.com>
2023-02-05 04:34:21 -0500

    removed enter key event handler.

--- client/script.js
+++ client/script.js
@@ -113,8 +113,3 @@ const handleSubmit = async (e) => {
 }
 
 form.addEventListener('submit', handleSubmit)
-form.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-        handleSubmit(e)
-    }
-})
